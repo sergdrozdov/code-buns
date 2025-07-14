a6360a602ce2d5063859e61a8ba75b934b6ac390
Sergey Drozdov <sergey.drozdov.1980@gmail.com>
2025-07-14 16:31:14 +0300

    Create DateTimeUtils and EnumHelper

---
+++ src/CodeBuns.Dotnet.Common/Helpers/EnumHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CodeBuns.Dotnet.Common.Helpers
+{
+    public static class EnumHelper<T> where T : struct
+    {
+        public static T Parse(string value)
+        {
+            T val;
+            if (Enum.TryParse(value, true, out val))
+                return val;
+
+            return default(T);
+        }
+
+        public static string GetName(object value)
+        {
+            return Enum.GetName(typeof(T), value);
+        }
+
+        public static TEnum GetDefaultValue<TEnum>() where TEnum : struct, Enum
+        {
+            return (TEnum)GetDefaultValue(typeof(TEnum));
+        }
+
+        public static object GetDefaultValue(Type enumType)
+        {
+            var attribute = enumType.GetCustomAttribute<DefaultValueAttribute>(inherit: false);
+            if (attribute != null)
+                return attribute.Value;
+
+            var innerType = enumType.GetEnumUnderlyingType();
+            var zero = Activator.CreateInstance(innerType);
+            if (enumType.IsEnumDefined(zero))
+                return zero;
+
+            var values = enumType.GetEnumValues();
+
+            return values.GetValue(0);
+        }
+    }
+}
---
+++ src/CodeBuns.Dotnet.Common/Utils/DateTimeUtils.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace CodeBuns.Dotnet.Common
+{
+    public static class DateTimeUtils
+    {
+        public static DateTime GetWeekFirstDay(DateTime date)
+        {
+            // Get the first day of the week (Monday)
+            var firstDayOfWeek = date.AddDays(-(int)date.DayOfWeek + (int)DayOfWeek.Monday);
+
+            return firstDayOfWeek.Date;
+        }
+
+        public static DateTime GetWeekFirstDay(int year, int month, int day)
+        {
+            if (year < 1 || month < 1 || month > 12 || day < 1 || day > DateTime.DaysInMonth(year, month))
+            {
+                throw new ArgumentOutOfRangeException("Invalid date provided.");
+            }
+
+            var date = new DateTime(year, month, day);
+
+            // Get the first day of the week (Monday)
+            var firstDayOfWeek = date.AddDays(-(int)date.DayOfWeek + (int)DayOfWeek.Monday);
+
+            return firstDayOfWeek.Date;
+        }
+
+        public static DateTime GetCurrentWeekFirstDay()
+        {
+            var date = DateTime.Now;
+
+            // Get the first day of the week (Monday)
+            var firstDayOfWeek = date.AddDays(-(int)date.DayOfWeek + (int)DayOfWeek.Monday);
+
+            return firstDayOfWeek.Date;
+        }
+
+        public static int GetWeekNumber(DateTime date)
+        {
+            var calendar = CultureInfo.CurrentCulture.Calendar;
+
+            var dateTimeFormat = CultureInfo.CurrentCulture.DateTimeFormat;
+            var firstDayOfWeek = dateTimeFormat.FirstDayOfWeek;
+            var calendarWeekRule = dateTimeFormat.CalendarWeekRule;
+
+            return calendar.GetWeekOfYear(date, calendarWeekRule, firstDayOfWeek);
+        }
+    }
+}
